0f4f79d4d741fa22bd29cea33cbc5d2c22a57287
Damir <damiros.20002@gmail.com>
2023-02-13 11:11:40 +0300

    Realised all tasks

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 "use strict";
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let personalMovieDB = {
-    count: +numberOfFilms,
+const personalMovieDB = {
+    count: numberOfFilms,
     movies:{},
     actors:{},
     genres:[],
     private: false
-}
+};
+
+const lastMovie1 = prompt('Один из последних просмотренных фильмов?', ''),
+    rating1 = +prompt('На сколько оцениваете его?',''),
+    lastMovie2 = prompt('Один из последних просмотренных фильмов?', ''),
+    rating2 = +prompt('На сколько оцениваете его?','');
+
+personalMovieDB.movies[lastMovie1] = rating1;
+personalMovieDB.movies[lastMovie2] = rating2;
